d91d944f971e992c2a929c3bcdc2e30261ef20da
Shrirang Pendke <shrirangpendkes@gmail.com>
2023-07-29 11:04:05 +0530

    change

--- srvr.py
+++ srvr.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def root():
-    return "welcome to ITIL exam v2"
+    return "welcome to ITIL exam"
 
 @app.route("/me", methods=["GET"])
 def root1():
